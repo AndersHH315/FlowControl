9c9fb86e5f88f21c52d83d32f8cff4987a0bcd57
Anders Hansen-Haug <andershh_93@hotmail.com>
2026-04-30 14:17:56 +0200

    Added main menu with only 1 option available and an infinity loop until correct input

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -5,7 +5,24 @@ namespace FlowControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string? input = "";
+            do
+            {
+                Console.WriteLine("------Main Menu------");
+                Console.WriteLine("Type 0 to exit the program!");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine("The program will close now!");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input!");
+                        break;
+                }
+            } while (input != "0");
         }
     }
 }
