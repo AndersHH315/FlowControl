a5b414f2c752f79fdfe1f51f8991b05e9b911dab
Anders Hansen-Haug <andershh_93@hotmail.com>
2026-04-30 15:18:56 +0200

    Added the possibility to check the cinema ticket price depending on age

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
+
 namespace FlowControl
 {
     class Program
@@ -7,10 +9,12 @@ namespace FlowControl
         {
 
             string? input = "";
+            string? age = "";
             do
             {
                 Console.WriteLine("------Main Menu------");
-                Console.WriteLine("Type 0 to exit the program!");
+                Console.WriteLine("Type 1 buy a cinema ticket");
+                Console.WriteLine("Type 0 to exit the program");
                 input = Console.ReadLine();
 
                 switch (input)
@@ -18,11 +22,35 @@ namespace FlowControl
                     case "0":
                         Console.WriteLine("The program will close now!");
                         break;
+                    case "1":
+                        Console.WriteLine("How old are you?");
+                        age = Console.ReadLine();
+                        while (int.TryParse(age, out int check) == false)
+                        {   
+                            Console.WriteLine("Type your age in numbers!");    
+                            age = Console.ReadLine();
+                        }
+                        int result = int.Parse(age);
+                        Console.WriteLine(checkAge(result));
+                        break;
                     default:
                         Console.WriteLine("Wrong input!");
                         break;
                 }
+                
             } while (input != "0");
+
+
+        }
+        static string checkAge(int age)
+        {
+
+            if(age < 20)
+                return "Ungdomspris: 80kr";                   
+            else if(age > 64)
+                return "Pensionärspris: 90kr";                   
+            else
+                return "Standardpris: 120kr";               
         }
     }
 }
